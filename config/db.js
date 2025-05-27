836bb612313d55eac7f065f9f8e46c7dde510755
Vivekcgv6 <vivekcgv6@gmail.com>
2025-05-27 13:38:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isRender = process.env.DATABASE_URL.includes('.render.com');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: isRender ? { rejectUnauthorized: false } : false, // Required for Render
 });
 
 pool.on('connect', () => {
-  console.log('Connected to the PostgreSQL database');
+  console.log('✅ Connected to PostgreSQL');
 });
 
 module.exports = {
