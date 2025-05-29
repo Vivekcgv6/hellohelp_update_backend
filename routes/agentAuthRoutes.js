ea4e4a38121e0ffc371f8f5a4d46bc1720fa138c
Vivekcgv6 <vivekcgv6@gmail.com>
2025-05-29 13:52:10 +0530

    Update agentAuthRoutes.js

--- routes/agentAuthRoutes.js
+++ routes/agentAuthRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getAgentInfo } = require('../controllers/agentAuthController');
+const { register, login, getAgentInfo, getAllAgents } = require('../controllers/agentAuthController');
 const authenticateToken = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/agent-info', authenticateToken, getAgentInfo);
+router.get('/agents', authenticateToken, getAllAgents);
 
 module.exports = router;
